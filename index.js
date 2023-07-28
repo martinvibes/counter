f744ede6d6eb51a9a3a0062d9c52c4b2574e9a58
martins <martinmachieb21@gmail.com>
2023-07-28 03:21:25 -1100

    changed somethings in the html and javascript

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
+const SAVE_ELEMENT = document.getElementById("save-el")
+const COUNT_ELEMENT = document.getElementById("count-el")
 let count = 0
 
-function increment() {
+function incrementCount() {
   count += 1
-  countEl.innerText = count
+  COUNT_ELEMENT.innerText = count
 }
 
-function sub() {
+function subtractCount() {
   if (count <= 0) {
     return 0
   }
   count -= 1
-  countEl.textContent = count
+  COUNT_ELEMENT.textContent = count
 }
 
-function save() {
+function saveCountValue() {
   let saved = count  + " - "
   let normal = 0
-  countEl.textContent = normal
+  COUNT_ELEMENT.textContent = normal
   count = 0
-  localStorage.setItem("count", saveEl.textContent += saved)
-  console.log( localStorage.getItem("count") )
+  localStorage.setItem("count", SAVE_ELEMENT.textContent += saved)
 }
 
